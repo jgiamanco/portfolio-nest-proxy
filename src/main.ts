74e6afa876f5cdcffeaa2a5362b1bbc4aaa89774
Jacob <jacobdeansd@gmail.com>
2025-03-16 02:33:11 -0700

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://jacobgiamanco.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:8080',
+      'https://jacobgiamanco.vercel.app',
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
