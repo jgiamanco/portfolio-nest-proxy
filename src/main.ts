a5e89305870dbf9e14bc7154b4262fd82fd1473e
Jacob <jacobdeansd@gmail.com>
2025-03-16 00:30:58 -0700

    Reverting

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   // Enable CORS
   app.enableCors({
     origin: ['http://localhost:3000', 'https://jacobgiamanco.vercel.app'],
@@ -14,11 +13,7 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 3000);
 }
 
-if (require.main === module) {
-  bootstrap().catch((error) => {
-    console.error('Failed to start server:', error);
-    process.exit(1);
-  });
-}
-
-export default bootstrap;
+bootstrap().catch((error) => {
+  console.error('Failed to start server:', error);
+  process.exit(1);
+});
